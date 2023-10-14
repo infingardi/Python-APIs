fa89b591f2f2f96cbc41ca75b5e7fe01e8ce79f8
igor <infingardidev@gmail.com>
2023-10-14 02:31:54 -0300

    Adicionando Apple nas lista

--- extraindo_dados.py
+++ extraindo_dados.py
@@ -9,7 +9,11 @@ ling_mais_usadas_netflix = netflix_rep.cria_df_linguagens()
 spotify_rep = DadosRepositorios('spotify')
 ling_mais_usadas_spotify = spotify_rep.cria_df_linguagens()
 
+apple_rep = DadosRepositorios('apple')
+ling_mais_usadas_apple = apple_rep.cria_df_linguagens()
+
 #Salvando os dados
 ling_mais_usadas_amzn.to_csv('dados/linguagens_amzn.csv')
 ling_mais_usadas_netflix.to_csv('dados/linguagens_netflix.csv')
 ling_mais_usadas_spotify.to_csv('dados/linguagens_spotify.csv')
+ling_mais_usadas_spotify.to_csv('dados/linguagens_apple.csv')
